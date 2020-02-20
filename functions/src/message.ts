6d6581aa2517bd468d7810f4b621783d0ae0ba62
Nilesh <nilesh.chauhan@pmcretail.com>
2020-02-20 19:25:38 +0530

    - resolve

--- functions/src/message.ts
+++
@@ -1,5 +0,0 @@
-import * as functions from 'firebase-functions';
-
-export const welcomeMessage = functions.https.onRequest((request, response) => {
-    response.send("Welcome to Firebase API!");
-});
